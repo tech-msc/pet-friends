7e934317413e078a82461e2891128c1563a034db
Matheus S. Cardoso <matheusscardoso5@gmail.com>
2019-05-25 10:07:42 -0300

    yarn install

--- src/CardList.js
+++ src/CardList.js
@@ -3,22 +3,17 @@ import React from 'react';
 import Card from './Card';
 
 const CardList = ({animalList}) => {
-    // const cardArray = animalList.map( (animal, id ) => {
-        //     console.log('------->' + id);
-        //     console.log('---animal ---->' + typeof(animalList) );
-        //     return <Card animal={animal} />
-        // });
+    
         
-        const cardArray = Object.keys(animalList).map(
+    const cardArray = Object.keys(animalList).map(
             function ( item ) {
-                return <Card animal={animalList[item] }  
-                //bug id key duplicated
-                key= { `${'cardList'}-${animalList[item].id }`  }      /> 
+                return (
+                    <Card animal={animalList[item] } key= { `${'cardList'}-${animalList[item].id }`  } /> 
+                ); 
             }
+        )
             
-            )
-            
-            // {debugger}
+    
     return (
 
         <div  className="flex justify-center flex-wrap">
