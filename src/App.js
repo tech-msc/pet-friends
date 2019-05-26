3de6fc5c48c09773f57a451cc89c215e541da7ee
tech-msc <23535116+tech-msc@users.noreply.github.com>
2019-05-26 20:33:22 -0300

    Add "loading"

--- src/App.js
+++ src/App.js
@@ -8,46 +8,20 @@ class App extends Component {
     super();
     this.state = {
       animals: [],
-      searchField: ""
-      //   animalsApi: []
+      searchField: "",
+      isLoading: false
     };
 
     console.log("CTOR");
   }
 
-  //   componentDidMount() {
-  //     let users = [] ;
-
-  //     var res = fetch('https://jsonplaceholder.typicode.com/users');
-  //     console.log("res");
-  //     console.log(res);
-
-  //    fetch('https://jsonplaceholder.typicode.com/users')
-  //       .then(response => {
-  //         response.json();
-  //         console.log(response);
-  //       })
-  //       .then( () => {
-  //         this.setState({ animals: users });
-
-  //       })
-  //       .catch((error) => {
-  //         console.error(error);
-  //       });
-
-  //       console.log( "Diiid" )
-  //   }
-
-  //   componentWillMount () {
-  //       console.log('wiiiiil')
-  //   }
-
   componentDidMount() {
+    this.setState ({ isLoading : true });
+
     fetch("https://jsonplaceholder.typicode.com/users")
       .then(response => response.json())
-      .then(data => this.setState({ animals: data }));
-    console.log("Did animals");
-    console.log(this.state.animals);
+      .then(data => this.setState({ animals: data, isLoading: false }));
+    
   }
 
   onSearchChange = event => {
@@ -55,55 +29,18 @@ class App extends Component {
   };
 
   render() {
-    // debugger;
-
-    // if(this.state.animals === null || this.state.animals === undefined ) {
-    //     // debugger;
-    //     this.state.animals = [];
-    // }
-
-    // const filteredwords = this.state.animals.filter(a => {
-
-    //     console.log( "FILTER" )
-
-    //     return a.name
-    //     .toLocaleLowerCase()
-    //     .includes(this.state.searchField.toLocaleLowerCase());
-    // });
-
-    //     const ress = ()=>
-    //     {
-    //         let animals1 = this.state.animals;
-    //         // if(animals1 === undefined) return animals1=[];
-    // debugger;
-    //             var res = animals1.filter(a=> {
-    //                 a.name.toLocaleLowerCase()
-    //                 .includes(this.state.searchField
-    //                     .toLocaleLowerCase())
-    //             });
-
-    //             return res;
-
-    //     }
-    //     const filteredwords = ress();
-
-    // const animalsIdNUll = () => {
-
-    //     let _animals = this.state.animals ;
-    //   if(_animals.length > 0){
-    //       debugger;
-    //       this.setState({ 'animals' : this.state.animals });
-    //       console.log('state -===-> ') ;
-    //       console.log(this.state.animals);
-    //       console.log('private -===-> ') ;
-    //       console.log(_animals);
-    //       return _animals;
-    //   }
-    // }
+    const { isLoading } = this.state;
+    if(isLoading) {
+      return (
+        <div className="tc">        
+          <h1 className="f2 site-title">Pet friends</h1>
+          <p className="f2 tc">Loading</p>
+        </div>
+      )
+    }
 
     const filteredwords = this.state.animals.filter(a => {
       console.log("FILTER");
-
       return a.name
         .toLocaleLowerCase()
         .includes(this.state.searchField.toLocaleLowerCase());
