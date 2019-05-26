5c80dcd58a041ea4460b015f824cd97c5c473a44
Matheus S. Cardoso <matheusscardoso5@gmail.com>
2019-05-26 19:44:38 -0300

    comment unused

--- src/Card.js
+++ src/Card.js
@@ -3,21 +3,23 @@ import React from "react";
 import "./Card.css";
 
 class Card extends React.Component {
-  renderImage(imageSrc) {
-    return (
-      <div>
-        <img src="#" alt="animal img" className="h4" />
-      </div>
-    );
-  }
+  // renderImage(imageSrc) {
+  //   return (
+  //     <div>
+  //       <img src="#" alt="animal img" className="h4" />
+  //     </div>
+  //   );
+  // }
 
   render() {
     let animal = this.props.animal;
     // debugger;
     return (
       <div className="br3 pa4 ma2 grow bw2 shadow-5 w5 card-images">
-        <div className="tc">
-          <h2>nome: {animal.name}</h2>
+        <div>
+          <h2>Id: {animal.id}</h2>
+          <h2>Name: {animal.name}</h2>
+          {/* <h2>Email: {animal.email}</h2> */}
         </div>
       </div>
     );
